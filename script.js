a28f795637ad49b1f80480ea4da2049f85f32216
Andre <erwin.ucelo@gmail.com>
2025-06-05 16:04:05 -0400

    Most of the javascript was finished

---
+++ script.js
@@ -0,0 +1,25 @@
+const emojis = [
+  "😍",
+  "😍",
+  "❤️",
+  "❤️",
+  "😵‍💫",
+  "😵‍💫",
+  "😭",
+  "😭",
+  "😎",
+  "😎",
+  "😃",
+  "😃",
+  "🤬",
+  "🤬",
+  "👍",
+  "👍",
+];
+var shuf_emojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+for (var i = 0; i < emojis.length; i++) {
+  let box = document.createElement("div");
+  box.className = "item";
+  box.innerHTML = shuf_emojis[i];
+  document.querySelector(".game").appendChild(box);
+}
